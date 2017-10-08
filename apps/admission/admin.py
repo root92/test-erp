7b135cf911808f424191dd8809f4a8af1323ea8e
Osman Jalloh <mudu.jalloh@gn.ehealthafrica.org>
2017-10-08 12:16:30 +0100

    feat: specify list_display.

--- apps/admission/admin.py
+++ apps/admission/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Registration
 
 
-admin.site.register(Registration)
+@admin.register(Registration)
+class RegistrationAdmin(admin.ModelAdmin):
+
+    list_display = ('first_name', 'last_name', 'student_card', 'department')
--- apps/departement/admin.py
+++ apps/departement/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Department, Course
 
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'department', 'level')
+
 admin.site.register(Department)
-admin.site.register(Course)
